b8c20cef67602a8849642208d1e9a769831c7d32
Izarothy <izarothmail@gmail.com>
2023-07-01 15:04:36 +0200

    style: make header shorter

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="grid h-screen place-items-center">
+    <header className="grid h-[80vh] place-items-center">
       <h1 className="text-center text-6xl font-bold">
         Hi, I&apos;m Michał <br />
         <span className="text-xl font-semibold">Frontend Developer</span>
