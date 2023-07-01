9e703c06e4c6927bacadc8a7df2271fdac6c623f
Izarothy <izarothmail@gmail.com>
2023-07-01 15:08:06 +0200

    style: add poppins

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,27 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import "~/styles/globals.css";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700", "800"],
+  style: ["normal", "italic"],
+  display: "swap",
+  subsets: ["latin"],
+});
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>
+        {`
+          html {
+            font-family: ${poppins.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
