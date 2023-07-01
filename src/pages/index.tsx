7ab6729505e801dd58fb41743d7b59f8feb41494
Izarothy <izarothmail@gmail.com>
2023-07-01 13:53:16 +0200

    chore: initial setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Izaroth - Portfolio</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]"></main>
+    </>
+  );
+}
