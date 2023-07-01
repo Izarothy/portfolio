acac5e198ec1fb85042dbd730a157d469eee0127
Izarothy <izarothmail@gmail.com>
2023-07-01 13:58:10 +0200

    feat: add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="grid h-screen place-items-center">
+      <h1 className="text-4xl font-bold">
+        Hi, I&apos;m Michał <br />
+        <span className="text-xl font-semibold">Frontend Developer</span>
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
