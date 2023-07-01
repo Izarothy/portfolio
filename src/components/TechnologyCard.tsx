8c4b376b911370116fbffc2fa923a9a43610c9b3
Izarothy <izarothmail@gmail.com>
2023-07-01 16:23:02 +0200

    style: make tech names bolder/more gray

--- src/components/TechnologyCard.tsx
+++ src/components/TechnologyCard.tsx
@@ -9,7 +9,7 @@ const TechnologyCard = ({ technologyList, title }: Props) => {
   return (
     <article className="min-h-full w-full rounded-md border border-secondary bg-dark py-8 text-center">
       <h3 className="mb-4 text-xl font-bold">{title}</h3>
-      <ul className="flex flex-col gap-1">
+      <ul className="flex flex-col gap-1 font-semibold text-gray-300">
         {technologyList?.map((technology) => {
           return <li key={technology}>{technology}</li>;
         })}
