f9660699f41374d3cd9d97bac9fb950cd43715d0
Izarothy <izarothmail@gmail.com>
2024-01-01 17:20:34 +0100

    feat: add background img to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,20 @@
+import Image from "next/image";
 import React from "react";
 
 const Header = () => {
   return (
-    <header className="grid h-screen place-items-center">
-      <h1 className="text-center text-6xl font-bold">
+    <header className="relative grid h-screen w-full place-items-center bg-opacity-30">
+      <Image
+        src="https://images.unsplash.com/photo-1610018556010-6a11691bc905?q=100&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        width={1170}
+        height={780}
+        quality={100}
+        className="relative h-full object-cover opacity-50"
+        alt="Background"
+      />
+      <h1 className="absolute text-center text-6xl font-bold">
         Hi, I&apos;m Michał <br />
-        <span className="text-xl font-semibold">Frontend Developer</span>
+        <span className="text-xl font-semibold">Web Developer</span>
       </h1>
     </header>
   );
