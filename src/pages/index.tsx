77412f47a1843bd13cf1f2d54a94b647ba37f1c4
Izarothy <izarothmail@gmail.com>
2024-01-10 00:46:57 +0100

    style: index widths, project font size

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,13 +12,13 @@ export default function Home() {
         <link rel="icon" href="/favicon-16.png" />
       </Head>
       <>
-        <main className="mx-auto mt-24 flex min-h-screen w-screen flex-col items-center px-4 sm:max-w-[50vw] xl:max-w-[33vw]">
+        <main className="mx-auto mt-24 flex min-h-screen w-screen max-w-[75vw] flex-col items-center px-4 xl:max-w-[33vw]">
           <Header />
 
           <section className="flex w-full flex-col py-16">
-            <h2 className="mb-4 text-left text-3xl font-semibold">Projects</h2>
+            <h2 className="mb-4 text-left text-xl font-semibold">Projects</h2>
 
-            <section className="flex flex-col gap-8 pt-16">
+            <section className="flex flex-col gap-8 pt-6">
               {projects?.map((project) => {
                 return <ProjectCard {...project} key={project.name} />;
               })}
