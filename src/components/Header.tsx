5e776aba1d695560e1c92304280ad34a39f07759
Izarothy <izarothmail@gmail.com>
2024-01-10 01:05:41 +0100

    feat: update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,13 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 const Header = () => {
   return (
     <header>
-      <h1 className="mb-4 font-semibold">Hi, I&apos;m Michał 👋</h1>
-      <p className="text-sm">
+      <h1 className="mb-4 text-lg font-semibold">Hi, I&apos;m Michał 👋</h1>
+      <p className="text-sm text-gray-200">
         I&apos;m a self-taught developer, who&apos;s mostly creating web stuff,
         although I also wrote a couple of Python scripts, Discord bots, a math
         library and was at some point bored enough to write a simple Fizzbuzz
-        program in ASM.
+        program in ASM. <br />
+        <br /> I love keeping things simple, aesthetic, functional and fast.
       </p>
       <div className="flex w-min gap-4 pt-4">
         <a href="https://github.com/Izarothy" className="hover:text-blue-200">
