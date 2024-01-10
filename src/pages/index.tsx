4e39b937dcbbba96cf9e2f76f609970babd027ea
Izarothy <izarothmail@gmail.com>
2024-01-10 01:54:34 +0100

    style: add same bg color to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon-16.png" />
       </Head>
       <>
-        <main className="mx-auto mt-24 flex min-h-screen w-screen max-w-[75vw] flex-col items-center px-4 xl:max-w-[33vw]">
+        <main className="mx-auto mt-24 flex min-h-screen w-screen max-w-[75vw] flex-col items-center bg-dark px-4 xl:max-w-[33vw]">
           <Header />
 
           <section className="flex w-full flex-col py-16">
